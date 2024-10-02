b15e943a79f4903b433a6b22e11282ecfeaa09b3
Arnav Ignatius <105193713+spartny@users.noreply.github.com>
2024-10-02 20:59:57 +0530

    create fencetrack

--- fenceTrack.py
+++ fenceTrack.py
@@ -2,16 +2,14 @@ import cv2
 from ultralytics import YOLO
 from fenceBuilder import checkInside, fenceBuild, drawFence
 
-# Load the YOLOv8 model
-model = YOLO('yolov8l.pt')
 
 # Open the video file
 
 # home day time video
-# video_path = "Videos\VIDEO_20240327_173849299.mp4"
+video_path = "Videos\VIDEO_20240327_173849299.mp4"
 
 # home night time video
-video_path = "Videos\VIDEO_20240327_173517186.mp4"
+# video_path = "Videos\VIDEO_20240327_173517186.mp4"
 
 # multi angle video
 # video_path = "Videos\VIDEO4.mp4"
@@ -20,18 +18,20 @@ video_path = "Videos\VIDEO_20240327_173517186.mp4"
 # video_path = "Videos\VIRAT_S_010204_05_000856_000890.mp4"
 
 # Nighttime Video
-video_path = "Videos\\NighttimeVideo.mp4"
+# video_path = "Videos\\NighttimeVideo.mp4"
 
+# load the YOLOv8 model
+model = YOLO('yolov8m.pt')
 cap = cv2.VideoCapture(video_path)
 
 frame_count = 0
-# Loop through the video frames
+# loop through the video frames
 while cap.isOpened():
-    # Read a frame from the video
+    # read a frame from the video
     success, frame = cap.read()
-
+    text_dim = int(len(frame) * 0.25)
     if success:
-        # Define the Virtual Fence for the Scene 1st Frame only
+        # define the Virtual Fence for the Scene 1st Frame only
         if frame_count == 0:
             fenceBuild(frame)
         frame_count += 1
@@ -41,8 +41,8 @@ while cap.isOpened():
 
             # draw fence over the frame
             drawFence(frame)
-            # Run YOLOv8 inference on the frame
-            results = model(frame, conf=0.5, classes=[0], iou=0.7)
+            # run YOLOv8 inference on the frame
+            results = model.track(frame, conf=0.6, classes=[0], persist=True, show=True, tracker="bytetrack.yaml")
             boxes = results[0].boxes.xyxy.tolist()  # get the bounding box co-ords
             outcome = False
             for box in boxes:
@@ -62,23 +62,23 @@ while cap.isOpened():
 
             if outcome:
                 color = (0, 0, 255)
-                cv2.putText(frame, 'BREACH', (300, 300), cv2.FONT_HERSHEY_SIMPLEX , 5, (0,0,255), 5, cv2.LINE_AA)
+                cv2.putText(frame, 'BREACH', (text_dim, text_dim), cv2.FONT_HERSHEY_SIMPLEX , 5, (0,0,255), 5, cv2.LINE_AA)
             else:
                 color = (0, 255, 0)
-                cv2.putText(frame, 'NO BREACH', (300, 300), cv2.FONT_HERSHEY_SIMPLEX , 5, (0,255,0), 5, cv2.LINE_AA)
-            # Visualize the results on the frame
+                cv2.putText(frame, 'NO BREACH', (text_dim, text_dim), cv2.FONT_HERSHEY_SIMPLEX , 5, (0,255,0), 5, cv2.LINE_AA)
+            # visualize the results on the frame
             annotated_frame = results[0].plot()
 
-            # Display the annotated frame
+            # display the annotated frame
             cv2.imshow("YOLOv8 Inference", annotated_frame)
 
-            # Break the loop if 'q' is pressed
+            # break the loop if 'q' is pressed
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
     else:
-        # Break the loop if the end of the video is reached
+        # break the loop if the end of the video is reached
         break
 
-# Release the video capture object and close the display window
+# release the video capture object and close the display window
 cap.release()
 cv2.destroyAllWindows()
